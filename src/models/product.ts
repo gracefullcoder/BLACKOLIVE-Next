80a753542b6391f4dab0a99670d5fab4e77fa0c2
Vaibhav <codingvaibhav247@gmail.com>
2025-01-05 09:42:10 +0530

    fdsaf

--- src/models/product.ts
+++ src/models/product.ts
@@ -34,6 +34,6 @@ const productSchema = new mongoose.Schema({
     ]
 })
 
-const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
