58b31eced5272680f4a92738f92a363bb990da0e
Vaibhav <codingvaibhav247@gmail.com>
2025-01-07 07:29:53 +0530

    trial

--- src/actions/Product.ts
+++ src/actions/Product.ts
@@ -1,10 +1,11 @@
-"use server"
+import connectToDatabase from "../lib/ConnectDb";
 import MembershipProd from "../models/membershipproducts"
 import Product from "../models/product";
 
 export const getSpecificProduct = async (id: any) => {
     try {
-        console.log("specfic product")
+        await connectToDatabase()
+        console.log("specfic product", id, "is")
         let product = await Product.findById(id);
 
         if (product == null) {
@@ -14,6 +15,38 @@ export const getSpecificProduct = async (id: any) => {
 
         return JSON.parse(JSON.stringify(product));
     } catch (error) {
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!idhar bidhu !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         console.log(error);
     }
 }
+
+
+export async function getProducts(type: string) {
+    try {
+        console.log("requested products");
+        await connectToDatabase()
+        const products = await Product.find();
+        const membership = await MembershipProd.find();
+        // const params = req.nextUrl.searchParams;
+
+        // const type = params.get('type');
+
+        console.log(products,membership)
+
+        if (type == "all") {
+            return JSON.parse(JSON.stringify({ products, membership }))
+        }
+        else if (type == "salads") {
+            return JSON.parse(JSON.stringify({ products }))
+
+        } else if (type == "membership") {
+            return JSON.parse(JSON.stringify({ membership }))
+        } else {
+            return JSON.parse(JSON.stringify({ message: "Not a valid request" }))
+        }
+    } catch (error) {
+        console.log(error)
+        return { error: "Failed to fetch data", status: 500 };
+    }
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
 import HeroSection from "@/src/components/HeroSection";
 import Products from "@/src/components/product/Products";
-import { productType } from "@/src/types/product";
+import { productType } from "../types/product";
 import axios from "axios";
+import { getProducts } from "../actions/Product";
 
-async function fetchProducts() {
-  try {
-    const response = await axios.get(`${process.env.API_BASE_URL}/api/product/?type=all`);
-
-    if (!response.status || response.status >= 400) {
-      throw new Error("Failed to fetch products");
-    }
-    return [...response.data.products, ...response.data.membership];
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return [];
-  }
-}
+// async function fetchProducts() {
+//   try {
+//     const response = await axios.get(`${process.env.API_BASE_URL}/api/product/?type=all`);
+//     if (!response.status || response.status >= 400) {
+//       throw new Error("Failed to fetch products");
+//     }
+//     return [...response.data.products, ...response.data.membership];
+//   } catch (error) {
+//     console.log("API_BASE_URL:", process.env.API_BASE_URL);
+//     console.error("Error fetching products:", error);
+//     return [];
+//   }
+// }
 
 export default async function Home() {
-  const products: productType[] = await fetchProducts();
+
+  const response = await getProducts("all");
+
+  const products = [...response.products, ...response.membership]
+
 
   return (
     <>
