4a89d8b24a50e16674a53c041310d880258e08dd
Vaibhav Gupta <139680548+gracefullcoder@users.noreply.github.com>
2025-01-14 12:29:51 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 export const config = {
     matcher: [
-        "/:admin*'
+        "/:function'
     ],
 };
 
